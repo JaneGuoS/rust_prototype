2d8819747e8d281fef30afed623cadb8a44724ea
Jane Guo <j.guo@seismic.com>
2025-01-08 14:49:58 +0800

    Complete the basic web functionalities

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
+use std::sync::Arc;
+
 use askama::Template;
 use axum::{
-    http::StatusCode,
-    response::{Html, IntoResponse, Response},
-    routing::get,
-    Router,
+    extract::State, http::StatusCode, response::{Html, IntoResponse, Response}, routing::{get, post}, Form, Router
 };
 
+use tokio::sync::Mutex;
 use tower_http::services::ServeDir;
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use serde::Deserialize;
+struct AppState {
+    todos: Mutex<Vec<String>>,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -25,17 +29,28 @@ async fn main() -> anyhow::Result<()> {
 
 
     //let router = Router::new().route("/", get(hello));
+    let app_state = Arc::new(AppState {
+        todos: Mutex::new(vec![]),
+    });
+
     let assets_path = std::env::current_dir().unwrap();
     let port = 8000_u16;
     let addr = std::net::SocketAddr::from(([0, 0, 0, 0], port));
 
+    let api_router = Router::new()
+        .route("/hello", get(hello_from_the_server))
+        .route("/todos", post(add_todo))
+        .with_state(app_state);
+
+
     let router = Router::new()
-    .route("/", get(hello))
-    .route("/another-page", get(another_page))
-    .nest_service(
-        "/assets",
-        ServeDir::new(format!("{}/assets", assets_path.to_str().unwrap())),
-    );
+        .nest("/api", api_router)
+        .route("/", get(hello))
+        .route("/another-page", get(another_page))
+        .nest_service(
+            "/assets",
+            ServeDir::new(format!("{}/assets", assets_path.to_str().unwrap())),
+        );
 
 
     info!("router initialized, now listening on port {}", port);
@@ -48,11 +63,39 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
+async fn hello_from_the_server() -> &'static str {
+    "Hello!"
+}
+
 async fn hello() -> impl IntoResponse {
     let template = HelloTemplate {};
     HtmlTemplate(template)
 }
 
+#[derive(Template)]
+#[template(path = "todo-list.html")]
+struct TodoList {
+    todos: Vec<String>,
+}
+
+#[derive(Deserialize)]
+struct TodoRequest {
+    todo: String,
+}
+
+#[axum::debug_handler]
+async fn add_todo(
+    State(state): State<Arc<AppState>>,
+    Form(todo): Form<TodoRequest>,
+) -> impl IntoResponse {
+    let mut lock = state.todos.lock().await;
+    lock.push(todo.todo);
+    let template = TodoList {
+        todos: lock.clone(),
+    };
+
+    HtmlTemplate(template)
+}
 
 #[derive(Template)]
 #[template(path = "hello.html")]
